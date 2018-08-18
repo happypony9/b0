8393eb659caa4d31c3b570d5ee644d192f4f0a36
buffer0verflow <39618280+happypony9@users.noreply.github.com>
2018-08-18 13:13:10 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
-echo "Installing dependencies..."
-apt-get install git nano screen python-argparse curl wget lynx unzip openvpn
-echo "Finished installing dependencies..."
+case $1 in
+install)  
+  echo "Installing b0..."
+  echo "Installing dependencies..."
+  apt-get install git nano screen python-argparse curl wget lynx unzip openvpn
+  echo "Finished installing dependencies..."
+  echo "Removing old installation..."
+  rm -rf /b0
+  echo "Download new/updated repository..."
+  cd /; git clone https://github.com/happypony9/b0.git /b0
+  ;;
+update)  
+  echo  "Updating b0..."
+  cd /b0; git pull
+  ;;
+update)  
+  echo  "Running b0..."
+  ;;
+*) 
+  echo "Must specify install, update or run!"
+  ;;
+esac
