c39ac5f67cffeee977f16187ed9cd9a5c061c7e2
buffer0verflow <39618280+happypony9@users.noreply.github.com>
2018-08-18 13:16:29 -0500

    Update and rename install.sh to b0.sh

--- b0.sh
+++ b0.sh
@@ -3,7 +3,7 @@ case $1 in
 install)  
   echo "Installing b0..."
   echo "Installing dependencies..."
-  apt-get install git nano screen python-argparse curl wget lynx unzip openvpn
+  apt-get install git nano screen python-argparse curl wget lynx unzip openvpn -y
   echo "Finished installing dependencies..."
   echo "Removing old installation..."
   rm -rf /b0
