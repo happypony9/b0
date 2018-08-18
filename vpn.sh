5d84dd6b805cd22b19d44905dab2889a94892c53
buffer0verflow <39618280+happypony9@users.noreply.github.com>
2018-08-18 16:50:12 -0500

    Update vpn.sh

--- vpn.sh
+++ vpn.sh
@@ -3,17 +3,21 @@ files=(/b0/vpn/*.ovpn)
 config="${files[RANDOM % ${#files[@]}]}"
 case $1 in
 start)
+  sudo /b0/keep.ssh > /b0/log 2>&1;
   sudo pkill -9 openvpn > /b0/log 2>&1;
   sudo screen -dmS vpn openvpn --config $config > /b0/log 2>&1;
   ;;
 stop) 
+  sudo /b0/keep.ssh > /b0/log 2>&1;
   sudo pkill -9 openvpn > /b0/log 2>&1;
   ;;
 restart)
+  sudo /b0/keep.ssh > /b0/log 2>&1;
   sudo pkill -9 openvpn > /b0/log 2>&1;
   sudo screen -dmS vpn openvpn --config $config > /b0/log 2>&1;
   ;;
 clear)  
+  sudo /b0/keep.ssh > /b0/log 2>&1;
   sudo pkill -9 openvpn > /b0/log 2>&1;
   sudo ip link delete tun0 > /b0/log 2>&1;
   ;;
