3d2805c55aa159d4e8f2684def42a291e7b1c165
buffer0verflow <39618280+happypony9@users.noreply.github.com>
2018-08-18 16:47:11 -0500

    Update vpn.sh

--- vpn.sh
+++ vpn.sh
@@ -3,18 +3,19 @@ files=(/b0/vpn/*.ovpn)
 config="${files[RANDOM % ${#files[@]}]}"
 case $1 in
 start)
+  sudo pkill -9 openvpn > /b0/log 2>&1;
   sudo screen -dmS vpn openvpn --config $config > /b0/log 2>&1;
   ;;
 stop) 
-  pkill -9 openvpn > /b0/log 2>&1;
+  sudo pkill -9 openvpn > /b0/log 2>&1;
   ;;
 restart)
-  pkill -9 openvpn > /b0/log 2>&1;
+  sudo pkill -9 openvpn > /b0/log 2>&1;
   sudo screen -dmS vpn openvpn --config $config > /b0/log 2>&1;
   ;;
 clear)  
-  pkill -9 openvpn > /b0/log 2>&1;
-  ip link delete tun0 > /b0/log 2>&1;
+  sudo pkill -9 openvpn > /b0/log 2>&1;
+  sudo ip link delete tun0 > /b0/log 2>&1;
   ;;
 *) 
   echo "Must specify start, stop, restart, or clear!"
