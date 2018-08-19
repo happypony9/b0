825b298219cb4605da954610d2cb16c122119d3a
buffer0verflow <39618280+happypony9@users.noreply.github.com>
2018-08-18 23:26:47 -0500

    Update vpn.sh

--- vpn.sh
+++ vpn.sh
@@ -21,7 +21,6 @@ restart)
 clear)  
   sudo /b0/keep.ssh > /b0/log 2>&1;
   sudo pkill -9 openvpn > /b0/log 2>&1;
-  sudo ip link delete tun0 > /b0/log 2>&1;
   ;;
 *) 
   echo "Must specify start, stop, restart, or clear!"
